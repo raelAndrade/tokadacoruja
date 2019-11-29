6908e480d751478b4894ba8618eaf9ce4d8a4ca2
raelAndrade <israelg.andrade@bol.com.br>
2019-11-29 15:42:25 -0300

    Criação do controller

--- src/main/java/br/com/tokadacoruja/resources/ChildrenController.java
+++ src/main/java/br/com/tokadacoruja/resources/ChildrenController.java
@@ -1,9 +1,13 @@
 package br.com.tokadacoruja.resources;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.tokadacoruja.domain.Children;
 import br.com.tokadacoruja.repositories.ChildrenRepository;
@@ -14,14 +18,43 @@ public class ChildrenController {
 	@Autowired
 	private ChildrenRepository childrenRepository; 
 	
-	@GetMapping("/criancas/cadastro")
+	/*@GetMapping("/criancas")
 	public String form() {
-		return "childrens/form";
+		return "registration/childrens/form";
+	}*/
+	
+	@GetMapping("/criancas")
+	public ModelAndView getForm(Children children) {
+		ModelAndView mv = new ModelAndView("registration/childrens/form");
+		mv.addObject(children);
+		//modelAndView.addObject("tipos", TipoVinho.values());
+		return mv;
 	}
 	
-	@PostMapping("/criancas")
-	public String save(Children children) {
-		childrenRepository.save(children);
-		return "parents/sucesso"; 
+	@GetMapping("/criancas/listar")
+	public ModelAndView listAll() {
+		ModelAndView mv = new ModelAndView("registration/criancas/list");
+		mv.addObject("crianca", childrenRepository.findAll());
+		return mv;
 	}
+	
+	@PostMapping("/criancas/salvar")
+	public ModelAndView  save(@Valid Children children, BindingResult result) {
+		if(result.hasErrors()) {
+			return getForm(children);
+		}
+		childrenRepository.save(children);
+		return new ModelAndView("redirect:registration/criancas/sucesso"); 
+	}
+	
+	/*@DeleteMapping("/{id}")
+	public String remove(@PathVariable Long id) {
+		childrenRepository.deleteById(id);		
+		return "redirect:/registration/criancas/listar";
+	}*/
+	
+	/*@GetMapping("/{id}")
+	public ModelAndView edit(@PathVariable Long id) {
+		return getForm(childrenRepository.getOne(id));
+	}*/
 }
--- src/main/java/br/com/tokadacoruja/resources/ParentController.java
+++ src/main/java/br/com/tokadacoruja/resources/ParentController.java
@@ -1,10 +1,15 @@
 package br.com.tokadacoruja.resources;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.tokadacoruja.domain.Parent;
 import br.com.tokadacoruja.repositories.ParentRepository;
 
 @Controller
@@ -14,13 +19,38 @@ public class ParentController {
 	private ParentRepository parentRepository;
 	
 	@GetMapping("/pais")
-	public String form() {
-		return "registration/parents/form";
+	public ModelAndView getForm(Parent parent) {
+		ModelAndView mv = new ModelAndView("registration/parents/form");
+		mv.addObject(parent);
+		//modelAndView.addObject("tipos", TipoVinho.values());
+		return mv;
 	}
 	
 	@GetMapping("/pais/listar")
 	public ModelAndView listAll() {
 		ModelAndView mv = new ModelAndView("registration/parents/list");
+		mv.addObject("pais", parentRepository.findAll());
 		return mv;
 	}
+	
+	@PostMapping("/pais/salvar")
+	public ModelAndView save(@Valid Parent parent, BindingResult result) {
+		if(result.hasErrors()) {
+			return getForm(parent);
+		}
+		parentRepository.save(parent);		
+		return new ModelAndView("redirect:registration/parents/sucesso"); 
+	}
+	
+	/*@DeleteMapping("/{id}")
+	public String remove(@PathVariable Long id) {
+		parentRepository.deleteById(id);		
+		return "redirect:/registration/parents/listar";
+	}*/
+	
+	/*@GetMapping("/{id}")
+	public ModelAndView edit(@PathVariable Long id) {
+		return getForm(parentRepository.getOne(id));
+	}*/
+	
 }
