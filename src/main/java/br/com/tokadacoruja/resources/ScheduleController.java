e9bda27dcd464d5041e182e2596e36cc465cb9f2
raelAndrade <israelg.andrade@bol.com.br>
2020-01-09 16:59:12 -0300

    Adicionando autocomplete

--- src/main/java/br/com/tokadacoruja/resources/ScheduleController.java
+++ src/main/java/br/com/tokadacoruja/resources/ScheduleController.java
@@ -4,6 +4,7 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.ResolverStyle;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -54,15 +55,15 @@ public class ScheduleController {
 		return mv;
 	}
 	
-	/*@GetMapping("/agendamentos/calendario")
+	@GetMapping("/agendamentos/agendar")
 	public ModelAndView getCalendar(Schedule schedule) {
-		ModelAndView mv = new ModelAndView("schedule/calendar");
+		ModelAndView mv = new ModelAndView("schedule/form");
 		List<Children> childrens = childrenRepository.findAll();
 		mv.addObject("payments", Payment.values());
 		mv.addObject("childrens", childrens);
 		mv.addObject("schedule", schedule);
 		return mv;
-	}*/
+	}
 	
 	@GetMapping("/agendamentos/listar")
 	public ModelAndView findAll() {
@@ -77,42 +78,15 @@ public class ScheduleController {
 			return form(schedule);
 		}	
 		schedule.setStatus(true);
-		//schedule.setTotalHours(show(schedule.getHourInitial(), schedule.getHourFinale()));
+		schedule.setTotalHours(show(schedule.getHourInitial(), schedule.getHourFinale()));
 		
-		/*LocalTime entry = schedule.getHourInitial();
-		LocalTime out = schedule.getHourFinale();
-		Duration duration = Duration.between(entry, out);
-		
-		schedule.setTotalHours(duration.toHours()+ ":" + duration.toMinutes());*/
-    	
-		String hour = schedule.getTotalHours().format(formatter);
-		String hours[] = hour.split("[.]");
-
-		Long h = Long.parseLong(hours[0]);
-		Long m = Long.parseLong(hours[1]);
-		
-		Double valuePerHours = 0.0;
-		Double valuePerMinutes = 0.0;
-		Double totalValuePerHours = 0.0;
-		
-    	if(h < 0 && m <= 59) {
-    		valuePerMinutes = schedule.getAmount();
-    		schedule.setAmount(valuePerMinutes);
-    	}else if(h >= 1 && m != 0) {
-    		valuePerHours = h * schedule.getAmount();
-    		valuePerMinutes = (schedule.getAmount() / 60.0) * m;
-    		totalValuePerHours = valuePerHours + valuePerMinutes;
-    		schedule.setAmount(totalValuePerHours);
-    	}else if(h >= 1 && m == 0){
-    		valuePerHours = h * schedule.getAmount();
-    		schedule.setAmount(valuePerHours);
-    	}
+		calculeOfHours(schedule);
 		scheduleService.save(schedule);
 		attributes.addFlashAttribute("mensagem", "Salvo com sucesso!");		
 		return new ModelAndView("redirect:/agendamentos/calendario");
 	}
-	
-	/*@GetMapping("/agendamentos/editar/{id}")
+
+	@GetMapping("/agendamentos/editar/{id}")
 	public ModelAndView edit(@PathVariable("id") Long id) {
 		Optional<Schedule> schedule = scheduleRepository.findById(id);
 		ModelAndView mv = new ModelAndView("schedule/form");
@@ -120,7 +94,7 @@ public class ScheduleController {
 		mv.addObject("childrens", childrens);
 		mv.addObject("schedule", schedule.get());
 		return mv;
-	}*/
+	}
 	
 	@GetMapping("/agendamentos/remover/{id}")
 	public ModelAndView remove(@PathVariable Long id){
@@ -145,10 +119,33 @@ public class ScheduleController {
         return wish.minusHours(now.getHour()).minusMinutes(now.getMinute());
     }
 
-    private LocalTime show(LocalTime schedule, String object) {
+	private LocalTime show(LocalTime schedule, String object) {
         LocalTime wish = LocalTime.parse(object, FORMAT_HOURS);
         LocalTime difference = missing(schedule, wish);
         return difference;
     }
+    
+    private void calculeOfHours(Schedule schedule) {
+		String hour = schedule.getTotalHours().format(formatter);
+		String hours[] = hour.split("[.]");
+		Long h = Long.parseLong(hours[0]);
+		Long m = Long.parseLong(hours[1]);
+		Double valuePerHours = 0.0;
+		Double valuePerMinutes = 0.0;
+		Double totalValuePerHours = 0.0;
+		
+    	if(h < 0 && m <= 59) {
+    		valuePerMinutes = schedule.getAmount();
+    		schedule.setAmount(valuePerMinutes);
+    	}else if(h >= 1 && m != 0) {
+    		valuePerHours = h * schedule.getAmount();
+    		valuePerMinutes = (schedule.getAmount() / 60.0) * m;
+    		totalValuePerHours = valuePerHours + valuePerMinutes;
+    		schedule.setAmount(totalValuePerHours);
+    	}else if(h >= 1 && m == 0){
+    		valuePerHours = h * schedule.getAmount();
+    		schedule.setAmount(valuePerHours);
+    	}
+	}
     
 }
