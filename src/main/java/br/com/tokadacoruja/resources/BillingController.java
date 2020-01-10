548cae53ca558c51a2169e0aa297fc0a9a1940b8
raelAndrade <israelg.andrade@bol.com.br>
2020-01-10 00:02:32 -0300

    correção controller billing

--- src/main/java/br/com/tokadacoruja/resources/BillingController.java
+++ src/main/java/br/com/tokadacoruja/resources/BillingController.java
@@ -1,36 +1,37 @@
 package br.com.tokadacoruja.resources;
 
-import java.time.LocalTime;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import br.com.tokadacoruja.domain.Schedule;
 import br.com.tokadacoruja.repositories.ChildrenRepository;
 import br.com.tokadacoruja.repositories.ScheduleRepository;
 
 @Controller
 public class BillingController {
 
+	@SuppressWarnings("unused")
 	@Autowired
 	private ScheduleRepository scheduleRepository;
 	
+	@SuppressWarnings("unused")
 	@Autowired
 	private ChildrenRepository childrenRepository;
 	
 	@GetMapping("/faturamento")
 	public ModelAndView form() {
 		ModelAndView mv = new ModelAndView("billings/list");
-		
-		Optional<Schedule> s = scheduleRepository.findById(1L);
-		
-		Schedule sch = new Schedule();
-		LocalTime dateInitial = sch.getHourInitial();
-		//String dateFinal = sch.getHourFinale();
-		Long id = sch.getId();
+		//Schedule schedule = (Schedule) scheduleRepository.buscaCriancaPorDataInicialEDataFinal(dateInitial, dateFinal, id);
+		//System.out.println("Total: " + schedule);
+		return mv;
+	}
+	
+	@PostMapping("/faturamento/{id}/{dateInitial}/{dateFinal}")
+	public ModelAndView filterPerData(@Param("id") Long id, @Param("dateInitial") String dateInitial, @Param("dateFinal") String dateFinal) {
+		ModelAndView mv = new ModelAndView("billings/list");
 		
 		//Schedule schedule = (Schedule) scheduleRepository.buscaCriancaPorDataInicialEDataFinal(dateInitial, dateFinal, id);
 		//System.out.println("Total: " + schedule);
